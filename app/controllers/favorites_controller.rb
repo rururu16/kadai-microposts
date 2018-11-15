dc3492bef73137200e1b3cfa7ae9e793f9ed5a0d
rururu <ynk618172@gmail.com>
2018-11-15 05:41:45 +0000

    fix favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,13 @@
 class FavoritesController < ApplicationController
   def create
-    post = Micropost.find(params[:favo])
+    post = Micropost.find(params[:id])
     current_user.favo(post)
     flash[:success] = 'いいねしました'
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    post = Micropost.find(params[:favo])
+    post = Micropost.find(params[:id])
     current_user.unfavo(post)
     flash[:success] = 'いいねを解除しました'
     redirect_back(fallback_location: root_path)
